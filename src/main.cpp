5cb4e3078d34138c32f466809744f39bef3bc10b
EtoAndruwa <timonina1909@gmail.com>
2023-09-16 22:26:11 +0300

    Setters/getters for the class vector are ready

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,23 @@ int main(int argc, char* argv[])
     // p1.print_point();
 
     vector v1(1, 2, 3, 5, 6, 7);
-    v1.print_vector();
+
+    coords_type x1 = 0;
+    coords_type y1 = 0;
+    coords_type z1 = 0;
+
+    coords_type x2 = 0;
+    coords_type y2 = 0;
+    coords_type z2 = 0;
+
+    v1.print_vector();  
+
+    printf("\n");
+    v1.getStart(x1, y1, z1);
+    printf("x1 = %lf, y1 = %lf, z1 = %lf\n", x1, y1, z1);
+    v1.getEnd(x2, y2, z2);
+    printf("x2 = %lf, y2 = %lf, z2 = %lf\n\n", x2, y2, z2);
+
     
  
     return 0;
